f41642a00eb6c3225aec12aadf0eddce2bb9ff21
Edmond Chuc <edmond.chuc@outlook.com>
2019-03-25 20:34:27 +1000

    Added path for database

--- database/__init__.py
+++ database/__init__.py
@@ -1,8 +1,10 @@
 import sqlite3
+from config import Config
+import os
 
 
 def connect(database_name):
-    conn = sqlite3.connect(database_name)
+    conn = sqlite3.connect(os.path.join(Config.APP_DIR, database_name))
 
     if not conn:
         raise sqlite3.DatabaseError('Error connecting to SQLite database "{}".'.format(database_name))
